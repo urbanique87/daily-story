a2df41c4733060592221976db6987027109981a9
urbanique <jungheeyov@gmail.com>
2024-11-04 17:50:10 +0900

    test: 질문 섹션 컴포넌트 기본 기능 구현 - 질문 제공 날짜, 질문 카테고리, 질문, 답변 페이지 이동 로직

--- src/components/question/QuestionSection.tsx
+++ src/components/question/QuestionSection.tsx
@@ -1,3 +1,39 @@
+import Link from "next/link"
+
+function formatCustomDate(dateString: string): string {
+  // 입력받은 문자열을 Date 객체로 변환
+  const date = new Date(dateString)
+
+  // 날짜 유효성 검사
+  if (isNaN(date.getTime())) {
+    throw new Error("유효하지 않은 날짜 형식입니다.")
+  }
+
+  const options: Intl.DateTimeFormatOptions = {
+    day: "2-digit",
+    month: "short",
+    year: "numeric",
+  }
+
+  return new Intl.DateTimeFormat("en-GB", options).format(date)
+}
+
+/**
+ * 질문 섹션
+ */
 export default function QuestionSection() {
-  return <></>
+  const id = 1
+  const category = "일상"
+  const question = "오늘, 가장 엉뚱했던 순간은 뭐였어?"
+
+  return (
+    <section>
+      <div>{formatCustomDate("2024-11-04")}</div>
+      <div>
+        오늘의 <span>{category}</span>
+      </div>
+      <p>{question}</p>
+      <Link href={`/question/${id}/answer`}>✍️ 여기를 눌러서 오늘의 이야기를 적어봐 🥰</Link>
+    </section>
+  )
 }

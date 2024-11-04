4555de6a29e3351e432169e4e4ea58678cbb99e2
urbanique <jungheeyov@gmail.com>
2024-11-04 15:49:36 +0900

    chore: 테스트 유저 프로필 이미지 변경

--- src/constants/test_user.ts
+++ src/constants/test_user.ts
@@ -1,4 +1,4 @@
 export const TEST_USER = {
-  profileImage: "https://example.com/profile.jpg",
+  profileImage: "https://i.pinimg.com/236x/24/a5/ee/24a5eeb64f5e14327850731fc8f01bd9.jpg",
   nickname: "홍길동",
 } as const

7ee0fd25b39b6ad1e5be71dddf952839373ec817
urbanique <jungheeyov@gmail.com>
2024-11-11 15:56:25 +0900

    chore: Next.js 설정 수정 - 콘솔 로그 제거 설정 추가
    - next.config.js 파일에 compiler 설정 추가
    - 제외 목록에 'error'를 설정하여 에러 로그는 유지되도록 구성

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
+  compiler: {
+    removeConsole: {
+      exclude: ["error"],
+    },
+  },
   /* config options here */
-};
+}
 
-export default nextConfig;
+export default nextConfig

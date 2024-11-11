c25306119e10479bad6e182401f7a6cfc3d84e39
JSON <jungheeyov@gmail.com>
2024-11-11 14:32:11 +0900

    feat: signup header component, auth menu ui 구현 (#23)

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,12 +1,17 @@
 import { SignupForm } from "@/components/auth/SignupForm"
+import Header from "@/components/common/Header"
 
 /**
  * 회원 가입 페이지
  */
 export default function SignupPage() {
   return (
-    <main>
-      <SignupForm />
-    </main>
+    <>
+      <Header />
+
+      <main className="max-w-[600px] px-5 py-7 mx-auto">
+        <SignupForm />
+      </main>
+    </>
   )
 }
